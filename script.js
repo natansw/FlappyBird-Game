5ec7f29805287cf39c3506b8d8a07645d0c03ef3
natansw <assaosawa@gmail.com>
2023-02-02 11:20:15 -0300

    passaro, chão e canos

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
 var canvas = document.getElementById('canvas').getContext("2d");
 
-var bg =  new Obj(0,0,500,900, "assets/images/sky.png");
+var bg =  new Bg(0,0,500,900, "assets/images/sky.png");
+var bg2 =  new Bg(500,0,500,900, "assets/images/sky.png");
+
+var ground = new Ground(0,700, 500, 200, "assets/images/ground.png");
+var ground2 = new Ground(500,700, 500, 200, "assets/images/ground.png");
+
+var pipe1 = new Pipe(300, 400, 96, 358, "assets/images/pipe1.png");
+var pipe2 = new Pipe(300, -100, 96, 358, "assets/images/pipe2.png");
+
+var bird = new Bird(50, 400, 63, 51, "assets/images/bird0.png");
+
+document.addEventListener("click", function(event){
+  bird.vel -= 12;
+});
 
 function draw() {
   bg.draw();
+  bg2.draw();
+  pipe1.draw();
+  pipe2.draw();
+  ground.draw();
+  ground2.draw();
+  bird.draw();
+
+
 }
 
 function update(){
+  bg.move(5, -500, 0);
+  bg2.move(5, 0, 500);
 
+  ground.move(2, -500, 0);
+  ground2.move(2, 0, 500);
+
+  bird.move();
+  bird.animation(8, 4, "bird");
+  bird.limits();
+
+  pipe1.move(1, -100, 600);
+  pipe2.move(1, -100, 600);
 }
 
 function main(){
   canvas.clearRect(0,0,500,900);
   draw();
   update();
+  requestAnimationFrame(main);
 }
 
-setInterval(main, 100);
+main();
--- util.js
+++ util.js
@@ -1,4 +1,8 @@
 class Obj{
+
+  frame = 0;
+  timer = 0;
+
   constructor(x,y,width,height, image){
     this.x = x;
     this.y = y;
@@ -13,4 +17,60 @@ class Obj{
     canvas.drawImage(img, this.x, this.y, this.width, this.height);
   }
 
+  animation(vel, limit, nome){
+    this.timer += 1;
+    if (this.timer >= vel) {
+      this.timer = 0;
+      this.frame += 1;
+    }
+    if (this.frame >= limit) {
+      this.frame = 0;
+    }
+
+    this.image = "assets/images/" + nome + this.frame + ".png";
+  }
+
+}
+
+class Bg extends Obj{
+  move(speed, limit, pos){
+    this.x -= speed;
+    if(this.x <= limit){
+      this.x = pos;
+    }
+  }
+}
+
+class Ground extends Bg{
+
+}
+
+class Bird extends Obj{
+
+  vel = 2;
+  grav = 1;
+
+  move(){
+    if(this.vel < 10){
+      this.vel += this.grav;
+    }
+    this.y += this.vel;
+  }
+
+  limits(){
+    if (this.y >= 660) {
+      this.y = 660;
+    }else if (this.y <= 0) {
+      this.y = 0;
+    }
+  }
+}
+
+class Pipe extends Obj{
+  move(vel, limit, new_pos){
+    this.x -= vel;
+    if (this.x <= limit){
+      this.x = new_pos;
+    }
+  }
 }
