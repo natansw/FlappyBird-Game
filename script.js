04f352001bdf3f08e1dfb2a9bc2f1d1b8d39d053
natansw <assaosawa@gmail.com>
2023-02-04 09:54:04 -0300

    ajustes finais

--- script.js
+++ script.js
@@ -11,9 +11,35 @@ var pipe2 = new Pipe(300, -100, 96, 358, "assets/images/pipe2.png");
 
 var bird = new Bird(50, 400, 63, 51, "assets/images/bird0.png");
 
-document.addEventListener("click", function(event){
-  bird.vel -= 12;
+var coin = new Coin(50,50, 45, 65, "assets/images/3.png");
+
+var play = true;
+
+var score = 0;
+var score_text = new Text();
+
+var fly = new Audio("assets/sounds/wing.ogg");
+var coin_pick = new Audio("assets/sounds/point.ogg");
+var gameover = new Audio("assets/sounds/hit.ogg");
+
+document.addEventListener("click", function(e){
+  bird.vel -= 15;
+  fly.play();
 });
+
+function colision() {
+  if (bird.collide(coin)) {
+    if (coin.set_visible) {
+      coin.set_visible = false;
+      score += 1;
+      coin_pick.play();
+    }}
+
+  if (bird.collide(pipe1) || bird.collide(pipe2)) {
+    gameover.play();
+    play = false;
+  }
+}
 
 function draw() {
   bg.draw();
@@ -23,23 +49,30 @@ function draw() {
   ground.draw();
   ground2.draw();
   bird.draw();
-
-
+  coin.draw();
+  score_text.draw_text(60, "Arial", 225, 220, "white");
 }
 
 function update(){
-  bg.move(5, -500, 0);
-  bg2.move(5, 0, 500);
+  if (play) {
+    bg.move(1, -500, 0);
+    bg2.move(1, 0, 500);
+    ground.move(2, -500, 0);
+    ground2.move(2, 0, 500);
+    bird.move();
+    bird.animation(8, 4, "bird");
+    bird.limits();
 
-  ground.move(2, -500, 0);
-  ground2.move(2, 0, 500);
+    pipe1.move(2, -100, 500 , pipe2);
 
-  bird.move();
-  bird.animation(8, 4, "bird");
-  bird.limits();
+    coin.move(pipe1);
+    coin.animation(8, 5, "");
 
-  pipe1.move(1, -100, 600);
-  pipe2.move(1, -100, 600);
+    score_text.text = score;
+
+    colision();
+
+  }
 }
 
 function main(){
--- util.js
+++ util.js
@@ -2,6 +2,7 @@ class Obj{
 
   frame = 0;
   timer = 0;
+  set_visible = true;
 
   constructor(x,y,width,height, image){
     this.x = x;
@@ -12,9 +13,11 @@ class Obj{
   }
 
   draw(){
-    var img = new Image();
-    img.src = this.image;
-    canvas.drawImage(img, this.x, this.y, this.width, this.height);
+    if (this.set_visible) {
+      var img = new Image();
+      img.src = this.image;
+      canvas.drawImage(img, this.x, this.y, this.width, this.height);
+    }
   }
 
   animation(vel, limit, nome){
@@ -30,6 +33,28 @@ class Obj{
     this.image = "assets/images/" + nome + this.frame + ".png";
   }
 
+  collide(obj){
+    if (this.x < obj.x + obj.width &&
+        this.x + this.width > obj.x &&
+        this.y < obj.y + obj.height &&
+        this.y + this.height > obj.y)
+        {
+          return true;
+        }else {
+          return false;
+        }
+  }
+
+}
+
+class Text{
+  text = "";
+
+  draw_text(size, font, x, y, color){
+    canvas.font = size + "px" + " " + font;
+    canvas.fillStyle = color;
+    canvas.fillText(this.text, x, y);
+  }
 }
 
 class Bg extends Obj{
@@ -67,10 +92,26 @@ class Bird extends Obj{
 }
 
 class Pipe extends Obj{
-  move(vel, limit, new_pos){
+  move(vel, limit, new_pos, pipe2){
     this.x -= vel;
     if (this.x <= limit){
       this.x = new_pos;
+      this.y = Math.random() * (600 - 400) + 400;
+    }
+
+    pipe2.x = this.x;
+    pipe2.y = this.y - 600;
+
+  }
+}
+
+class Coin extends Obj{
+  move(pipe){
+    this.x = pipe.x + 25;
+    this.y = pipe.y - 150;
+
+    if (this.x <= -50) {
+      this.set_visible = true;
     }
   }
 }
